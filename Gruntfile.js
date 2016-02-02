05a965f197606ed601c3ef54120bae101ebeed47
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-02-02 16:07:00 +0100

    from tabs to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,92 +1,92 @@
 module.exports = function (grunt) {
 
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-        browserSync: {
-            dev:{
-                bsFiles: {
-                    src : [ 
-                        '*.html',
-                        'assets/css/*.css',
-                        'assets/js/*.js'
-                    ]
-                },
-                options: {
-                    server: {
-                        baseDir: "./",
-                    },
-                    watchTask: false,
-                    open: false
-                }
-            }
+  grunt.initConfig({
+    browserSync: {
+      dev:{
+        bsFiles: {
+          src : [ 
+          '*.html',
+          'assets/css/*.css',
+          'assets/js/*.js'
+          ]
         },
-        useminPrepare: {
-            html: 'index.html',
-            options: {
-              dest: 'public'
-            }
-        },
-        usemin:{
-          html:['public/index.html']
-        },
-        copy:{
-          html: {
-              src: './index.html', dest: 'public/index.html'
+        options: {
+          server: {
+            baseDir: "./",
           },
-          assets_dir: {
-              src: 'assets/', dest: 'public/'
-          },
-          img: {
-              src: 'assets/img/*', dest: 'public/'
-          },
-          font: {
-              src: 'assets/font/*', dest: 'public/'
-          },
-          text: {
-              src: 'assets/text/*', dest: 'public/'
-          },
-          svg: {
-              src: 'assets/svg/*', dest: 'public/'
-          }
-        },
-        postcss: {
-          options: {
-            map: false, // inline sourcemaps
+          watchTask: false,
+          open: false
+        }
+      }
+    },
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: 'public'
+      }
+    },
+    usemin:{
+      html:['public/index.html']
+    },
+    copy:{
+      html: {
+        src: './index.html', dest: 'public/index.html'
+      },
+      assets_dir: {
+        src: 'assets/', dest: 'public/'
+      },
+      img: {
+        src: 'assets/img/*', dest: 'public/'
+      },
+      font: {
+        src: 'assets/font/*', dest: 'public/'
+      },
+      text: {
+        src: 'assets/text/*', dest: 'public/'
+      },
+      svg: {
+        src: 'assets/svg/*', dest: 'public/'
+      }
+    },
+    postcss: {
+      options: {
+        map: false, // inline sourcemaps
 
-            processors: [
-              //require('pixrem')(), // add fallbacks for rem units
-              require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
-              //require('cssnano')() // minify the result
-            ]
-          },
-          dist: {
-            src: 'assets/css/*.css'
-          }
-        },
-        watch: {
-          scripts: {
-            files: 'assets/css/*.css',
-            tasks: ['postcss'],
-          },
-        },
-    });
-    
+        processors: [
+          //require('pixrem')(), // add fallbacks for rem units
+          require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+          //require('cssnano')() // minify the result
+        ]
+      },
+      dist: {
+        src: 'assets/css/*.css'
+      }
+    },
+    watch: {
+      scripts: {
+        files: 'assets/css/*.css',
+        tasks: ['postcss'],
+      },
+    },
+  });
 
-    grunt.registerTask('dev', ['browserSync:dev']);
-    grunt.registerTask('build',[
-      'copy:html',
-      'copy:assets_dir',
-      'copy:font',
-      'copy:img',
-      'copy:text',
-      'copy:svg',
-      'postcss',
-      'useminPrepare',
-      'concat',
-      'uglify',
-      'cssmin',
-      'usemin'
+
+  grunt.registerTask('dev', ['browserSync:dev']);
+  grunt.registerTask('build',[
+    'copy:html',
+    'copy:assets_dir',
+    'copy:font',
+    'copy:img',
+    'copy:text',
+    'copy:svg',
+    'postcss',
+    'useminPrepare',
+    'concat',
+    'uglify',
+    'cssmin',
+    'usemin'
     ]);
 
 };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
  $(document).ready(function(){
-	$("#input_field").on('input',function(e){
-		// console.log($("#input_field").val())
-		$(".traslation").text($("#input_field").val())
-		// $(".traslation").text('&#101;')
-	})
+  $("#input_field").on('input',function(e){
+    // console.log($("#input_field").val())
+    $(".traslation").text($("#input_field").val())
+    // $(".traslation").text('&#101;')
+  })
 })
