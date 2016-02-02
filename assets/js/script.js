a52d772a99e5e7c3f616acea54d88073ab89ed0f
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-02-02 16:08:07 +0100

    first prototype

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
  $(document).ready(function(){
   $("#input_field").on('input',function(e){
     // console.log($("#input_field").val())
-    $(".traslation").text($("#input_field").val())
+    // $(".traslation").text($("#input_field").val())
     // $(".traslation").text('&#101;')
+    
+    var cleartext = $("#input_field").val()
+    var ciphred = caesarShift(cleartext, 3)
+    var emojied = emojiEncode(ciphred)
+    $(".traslation").html(emojied.replace(/\n/g,"<br>"))
+    $(".backfromtheemoji").html(emojiDecode(emojied).replace(/\n/g,"<br>"))
+    $(".backfromthepast").html(caesarShift(emojiDecode(emojied), -3).replace(/\n/g,"<br>"))
   })
 })
