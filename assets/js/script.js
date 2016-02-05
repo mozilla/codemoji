f468f34184cc719ce2d0ca1efb8288ab19f40349
Amedeo Spagnolo <a.spagnolo@todo.to.it>
2016-02-05 16:40:15 +0100

    logic key click+

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,16 @@
       $('.main_content_bottom_input').text(Criptoloji.encode(text))
   })
 
+
+
+  var emoji_list = ['a','b','c','d','e','f','g','a','b','c','d','e','f','g']
+
+  _.each(emoji_list, function(d){
+    $(".keyslider_content").append('<p class="key" key="'+d+'">'+d+'</p>')
+  })
   $('.key').click(function(){
     console.log($(this).attr('key'))
+    $('.main_content_bottom_input').text($(this).attr('key'))
   })
 
 })
