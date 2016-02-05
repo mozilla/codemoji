1e7f77f4e25b78fdcb733a6f520cd4f69b49d830
Amedeo Spagnolo <a.spagnolo@todo.to.it>
2016-02-05 15:58:40 +0100

    setup logi main section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
  $(document).ready(function(){
   $("body").height($(window).innerHeight())
+
+  $('.main_content_top_input').bind('input propertychange', function() {
+    var text = $('.main_content_top_input').val()
+      $('.main_content_bottom_input').text(emojiEncode(text))
+  })
+
 })
+
+
+
+
+
