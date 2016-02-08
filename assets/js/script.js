9641904b4569c6f0c3fbbcb05812ef40acd159b7
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-02-08 19:06:53 +0100

    add keyslider custom slider logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,14 +25,41 @@
     console.log('Chosen key', key)
     $('.main_content_bottom_input').text(Criptoloji.encrypt(text))
   })
-
-  //
-  // Key Slider
-  //
-
 })
 
 function toSection(button){
   $(".section").hide()
   $(button).show()
 }
+
+//
+// Key Slider
+//
+function keysliderGoto(toElem, elementWidth) {
+  $('.keyslider').animate({scrollLeft: toElem*elementWidth}, 200)
+}
+var debouncedKeysliderGoto = _.debounce(keysliderGoto, 200)
+var lastPos = 0
+var $wrapper = $('.keyslider')
+$('.keyslider').scroll(function (event) {
+  // debugger;
+  var elementWidth = $('.key:nth-child(1)', $wrapper).outerWidth(true)
+  console.log('elwidth', elementWidth)
+  var currPos = $wrapper.scrollLeft()
+  var direction = (currPos > lastPos) ? 'right' : 'left'
+  console.log('keyslider direction: ' + direction)
+  // calculate proper slide to element    
+  var toElem = (currPos / elementWidth)
+  var rightThreshold = 0.5
+  var leftThreshold = 0.75
+  var decimal = (toElem % 1).toFixed(1)
+  console.log(toElem, decimal)
+  if (direction === 'right' && decimal > rightThreshold)  
+    toElem += 1
+  if (direction === 'left' && decimal > 0 && decimal < leftThreshold)  
+    toElem -= 1
+  toElem = Math.ceil(toElem)
+  console.log('keyslider goTo', toElem)
+  debouncedKeysliderGoto(toElem, elementWidth)
+  lastPos = currPos
+});
