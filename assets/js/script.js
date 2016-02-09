48b1bbd28a79a57c5540b59d56515ade17bea760
Amedeo Spagnolo <a.spagnolo@todo.to.it>
2016-02-09 13:34:43 +0100

    add modal emoji

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
  $(document).ready(function(){
 
-  toSection(".section_share")
+  toSection(".section_main")
 
   $("body").css("height",$(window).innerHeight())
   $(".main_content_top").css("height",$(window).innerHeight()/2-$(".main_keyslider").height()/2)
+  $(".main_key_modal").css("height",$(window).innerHeight()/2-$(".main_keyslider").height()/2)
   $(".main_content_bottom").css("height",$(window).innerHeight()/2-$(".main_keyslider").height()/2)
   $(".main_content_top_label").css("margin-top", $(".header").height())
 
@@ -12,7 +13,10 @@
   //
   var emoji_list = ['😹','😤','😐','😖','😀','😻','😕','🙍','😠','😨','😘','😇','😄','😂','😹','😤','😐','😖','😀','😻','😕','🙍','😠','😨','😘','😇','😄','😂']
   _.each(emoji_list, function(d){
-    $(".keyslider_content").append('<p class="key" key="'+d+'">'+twemoji.parse(d)+'</p>')
+    $(".key_modal").before('<p class="key" key="'+d+'">'+twemoji.parse(d)+'</p>')
+  })
+  _.each(emoji_list, function(d){
+    $(".main_key_modal_1").append('<p class="key" key="'+d+'">'+d+'</p>')
   })
   $('.main_content_top_input').bind('input propertychange', function() {
     encryptText()
@@ -28,6 +32,25 @@
     var elementWidth = $('.key:nth-child(1)', $wrapper).outerWidth(true)
     var toElem = $('.key').index(this)
     keysliderGoto(toElem, elementWidth)
+  })
+
+
+  // modal
+  $("body").click(function(event) { 
+    console.log(event)
+    console.log(event.target)
+    console.log(event.target.closest('.main_key_modal'))
+    if($('body').hasClass('key-modal-open')  && !$(event.target.closest('.main_key_modal')).is('.main_key_modal')) {
+        $(".main_key_modal").hide()
+        $('body').removeClass('key-modal-open')
+        console.log(event.target)
+    }        
+  })
+
+  $('.key_modal').click(function(event){
+    event.stopPropagation()
+      $(".main_key_modal").show()
+      $('body').addClass('key-modal-open')
   })
 })
 
