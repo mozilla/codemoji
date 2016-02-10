fc6a38fdad4b15d31660873219bc5b83f24bae58
Amedeo Spagnolo <a.spagnolo@todo.to.it>
2016-02-10 16:34:09 +0100

    logic flow onboarding

--- assets/js/states/onboarding.js
+++ assets/js/states/onboarding.js
@@ -6,27 +6,31 @@
   Cryptoloji.states.onboarding.root = {
     enter: function () {
       $(".section_onboarding").addClass("section-show")
+      $(".header").hide()
     },
     leave: function () {
       $(".section_onboarding").removeClass("section-show")
+      $(".header").show()
     }
   }
 
   Cryptoloji.states.onboarding.watermelon = {
     enter: function () {
-      $(".section_onboarding").addClass("section-show")
+      $(".section_onboarding").addClass("section-show").addClass("step-0")
+      $(".header").hide()
+      var i = 0
+      $(".section_onboarding").on("click", function(){
+         if(i < 4){
+           $(".section_onboarding").removeClass("step-"+ parseInt(i)).addClass("step-"+ parseInt(i + 1))
+           i = i + 1
+         } else {
+          Cryptoloji.stateman.go("home")
+         }
+      })
     },
     leave: function () {
       $(".section_onboarding").removeClass("section-show")
-    }
-  }
-
-  Cryptoloji.states.onboarding.apple = {
-    enter: function () {
-      $(".section_onboarding").addClass("section-show")
-    },
-    leave: function () {
-      $(".section_onboarding").removeClass("section-show")
+      $(".header").show()
     }
   }
 
