44fa42d96c6e9a3cb2eaf4403bf19d2a9b4c83c9
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-02-10 15:08:18 +0100

    remove html5 history support

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,7 +47,7 @@
   //
   // StateMan bootstrap
   //
-  Cryptoloji.stateman.start({ html5: true, prefix: '!' })
+  Cryptoloji.stateman.start()
   // go to welcome if no state is active
   if (Cryptoloji.stateman.current.name === '')
     Cryptoloji.stateman.go('welcome')
