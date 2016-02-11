494ff3d3113f4dbe4ed571252f65abcfb8f18275
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-02-11 17:30:59 +0100

    show header on home state

--- assets/js/states/home.js
+++ assets/js/states/home.js
@@ -4,6 +4,7 @@
   Cryptoloji.states.home = {
     enter: function () {
       $(".section_home").addClass("section-show")
+      Cryptoloji.stateman.emit('header:show')
     },
     leave: function () {
       $(".section_home").removeClass("section-show")
