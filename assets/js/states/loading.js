293886634f668aeece23c5c925a120fbb178766c
abusedmedia <ff@todo.to.it>
2016-02-11 13:05:36 +0100

    txt loading

--- assets/js/states/loading.js
+++ assets/js/states/loading.js
@@ -5,6 +5,10 @@
     enter: function () {
       $(".section_loading").addClass("section-show")
       $(".header").hide()
+
+      setTimeout(function(){
+        Cryptoloji.stateman.go('home')
+      }, 5000)
     },
     leave: function () {
       $(".section_loading").removeClass("section-show")
