15004654460c99b27603f86203bc75914351aaf8
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-02-11 15:38:33 +0100

    animate encryption input

--- assets/js/states/encrypt.js
+++ assets/js/states/encrypt.js
@@ -5,20 +5,8 @@
     enter: function () {
       $(".encryption").addClass("section-show")
 
-      var encryptionInputPlaceholder = 'Type here your message'
-      $('#encryption_input').typed({
-        strings: [encryptionInputPlaceholder, '^'],
-        typeSpeed: 10,
-        contentType: 'text',
-        showCursor: false,
-        backSpeed: 10,
-        backDelay: 1000,
-        // attr: 'placeholder',
-        callback: function () {
-          $('#encryption_input').attr('placeholder', encryptionInputPlaceholder)
-          $('#encryption_input').text('')
-        }
-      })
+      // animate input placeholder text
+      animateInputPlaceholder()
 
       $('.encryption .main_content_top_input').bind('input propertychange', function() {
         Cryptoloji.UI.encryptText()
@@ -33,6 +21,25 @@
       $('.encryption').removeClass('section-show')
       Cryptoloji.stateman.off('encrypt')
     }
+  }
+
+  function animateInputPlaceholder () {
+    // store current placeholder
+    var encryptionInputPlaceholder = $('#encryption_input').attr('placeholder')
+    // remove it for a cleaner start
+    $('#encryption_input').attr('placeholder', '')
+    // typify placeholder
+    $('#encryption_input').typed({
+      strings: [encryptionInputPlaceholder],
+      typeSpeed: 10,
+      contentType: 'text',
+      showCursor: false,
+      attr: 'placeholder',
+      callback: function () {
+        // everything back to normal please
+        setTimeout(function () { $('body').removeClass('main_content_top_input-first') }, 1000)          
+      }
+    })
   }
 
 })(window, window.Cryptoloji); 
