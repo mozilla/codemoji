b17b97f68f1cdc20e9f1ce5a2fd565d0730aee9f
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-02-12 17:01:10 +0100

    fix decrypt url + unregister listeners

--- assets/js/states/decrypt.js
+++ assets/js/states/decrypt.js
@@ -7,19 +7,19 @@
   var wrongKey = false
 
   Cryptoloji.states.decrypt = {
-    enter: function () {
+    url: '/decrypt/:id',
+    enter: function (options) {
       Cryptoloji.stateman.emit('header:show')
       $(".decryption").addClass("section-show")
 
-      // will be retrieved from promise
+      // will be retrieved from promise via param.id
       Cryptoloji.UI.showDecryptableText('😱😥😦😰 😦😰 😸 😪😢😰😰😸😤😢')
       correctKey = '😻'
-
+      
       // show reply button at proper time
       Cryptoloji.stateman.on('decrypt:show-reply', function() {
-        if (!wrongKey) {
+        if (!wrongKey)
           $('#decryption_reply_button').fadeIn()
-        }
       })
 
       // notify wrong key
@@ -33,15 +33,23 @@
         }
       })
 
+      // wrong key handler
       Cryptoloji.stateman.on('decrypt:wrong-key', function () {
         console.log('wrong key')
       })
+
+      // right key handler
       Cryptoloji.stateman.on('decrypt:right-key', function () {
         console.log('right key')
       })
     },
     leave: function () {
       $(".decryption").removeClass("section-show")
+      // unregister listeners
+      Cryptoloji.stateman.off('decrypt:show-reply')
+      Cryptoloji.stateman.off('decrypt:key-chosen')
+      Cryptoloji.stateman.off('decrypt:wrong-key')
+      Cryptoloji.stateman.off('decrypt:right-key')
     }
   }
 
