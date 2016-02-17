3d218f01b8f6c96816b3271be290afa2ca49bd54
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-02-17 19:02:30 +0100

    implement share link copy to clipboard

--- assets/js/states/share.js
+++ assets/js/states/share.js
@@ -1,5 +1,7 @@
 (function (window, Cryptoloji, undefined) {
   'use strict'
+
+  var clipboard = null
   
   Cryptoloji.states.share = {
     enter: function () {
@@ -8,15 +10,37 @@
         $("#share_more_arrow").addClass("shown")
       }, 0)
 
-      insertShareMessage()
+      clipboard = new Clipboard('#share_copytoclipboard')
+      fillLinkCopy()
+      copyToClipboardHandler()
     },
     leave: function () {
       $(".section_share").removeClass("section-show")
+      clipboard.destroy()
     }
   }
 
-  function insertShareMessage () {
-    $('#share_content').html(Cryptoloji.UI.toTwemoji(Cryptoloji.current.output))
+  function copyToClipboardHandler () {
+    clipboard.on('success', function(e) {
+      e.clearSelection()
+
+      // give feedback in place
+      var oldVal = $(e.trigger).val()
+      $(e.trigger).val('Copied!')
+      setTimeout(function () {
+        $(e.trigger).val(oldVal)
+      }, 350)
+    });
+
+    clipboard.on('error', function(e) {
+      alert('cannot copy')
+      console.error('Action:', e.action)
+      console.error('Trigger:', e.trigger)
+    });
+  }
+
+  function fillLinkCopy () {
+    // fill copy to clipboard link input
   }
 
 })(window, window.Cryptoloji); 
