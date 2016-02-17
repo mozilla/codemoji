670e28d9b844a4b4232cd885102d5da19b6e9588
Amedeo Spagnolo <a.spagnolo@todo.to.it>
2016-02-17 13:09:15 +0100

    fix share section

--- assets/js/states/share.js
+++ assets/js/states/share.js
@@ -4,6 +4,10 @@
   Cryptoloji.states.share = {
     enter: function () {
       $(".section_share").addClass("section-show")
+      setTimeout(function(){
+        $("#share_more_arrow").addClass("shown")
+      }, 0)
+      // $("#share_more_arrow").addClass("shown")
     },
     leave: function () {
       $(".section_share").removeClass("section-show")
