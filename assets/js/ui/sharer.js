222e3478b49db77fb6059d1988612d31951c55c4
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-02-18 12:33:58 +0100

    add Cryptoloji.UI.Sharer

---
+++ assets/js/ui/sharer.js
@@ -0,0 +1,83 @@
+(function (window, Cryptoloji, $, undefined) {
+  
+  var sharers = {}
+
+  Cryptoloji.UI.Sharer = function createSharer (name, selector) {
+    if (!sharers[name]) 
+      sharers[name] = new Sharer(name, selector)
+    return sharers[name]
+  }
+
+  function Sharer (name, selector) {
+    var self = this
+    self.name = name
+    self.$element = $(selector)
+
+    self.openWindow = true
+    self.shareLink = null
+    self.params = {}
+
+    return self
+  }
+
+  Sharer.prototype.addParam = function addParam (name, value) {
+    var self = this
+    self.params[name] = value
+    return self
+  }
+
+  Sharer.prototype.bind = function bind () {
+    var self = this
+    self.$element.on('click', self.onClick.bind(self))
+    return self
+  }
+
+  Sharer.prototype.onClick = function onClick (event) {
+    var self = this
+
+    if (self.shareLink) {
+      var i = 575,
+          n = 350,
+          r = screen.width / 2 - i / 2,
+          s = screen.height / 2 - n / 2;
+
+      console.log()
+      var link = self.shareLink + '?'
+      var keys = _.keys(self.params)
+      _.each(keys, function (key, idx) {
+        link += key + '=' + encodeURIComponent(self.params[key])
+        if (idx < keys.length - 1)
+          link += '&'
+      })
+      console.debug('share link:', link)
+      if (self.openWindow) {
+        event.preventDefault()
+        window.open(link, 
+          "_blank", 
+          "height=" + n + ",width=" + i + ",top=" + s + ",left=" + r)
+      } else {
+        self.$element.attr('href', link)
+      }
+    }
+
+    return self
+  }
+
+  Sharer.prototype.sameWindow = function sameWindow () {
+    var self = this
+    self.openWindow = false
+    return self
+  }
+
+  Sharer.prototype.setShareLink = function setShareLink (link) {
+    var self = this
+    self.shareLink = link
+    return self
+  }
+
+  Sharer.prototype.unbind = function unbind () {
+    var self = this
+    self.$element.off()
+  }
+
+})(window, window.Cryptoloji, window.jQuery); 
