16688b1a629bcde9e55d9ea6249a785b21dd9d0c
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-02-18 12:34:40 +0100

    fix share buttons behaviour

--- assets/js/states/share.js
+++ assets/js/states/share.js
@@ -3,7 +3,7 @@
 
   var linkClipboard = null
   var keyClipboard = null
-  
+
   Cryptoloji.states.share = {
     enter: function () {
       $(".section_share").addClass("section-show")
@@ -18,10 +18,20 @@
       keyClipboard = new Clipboard('#share_copykeytoclipboard')
       copyLinkToClipboardHandler()
       copyKeyToClipboardHandler()
+
+      setupShareButtons()
     },
     leave: function () {
       $(".section_share").removeClass("section-show")
       linkClipboard.destroy()
+
+      // sharer cleanup
+      Cryptoloji.UI.Sharer('facebook').unbind()
+      Cryptoloji.UI.Sharer('twitter').unbind()
+      Cryptoloji.UI.Sharer('gplus').unbind()
+      // Cryptoloji.UI.Sharer('tumblr').unbind()
+      Cryptoloji.UI.Sharer('whatsapp').unbind()
+      Cryptoloji.UI.Sharer('mail').unbind()
     }
   }
 
@@ -65,6 +75,40 @@
 
   function fillLinkCopy () {
     // fill copy to clipboard link input
+  }
+
+  function setupShareButtons () {
+    Cryptoloji.UI.Sharer('facebook', '#share_button_fb')
+      .setShareLink('https://www.facebook.com/sharer/sharer.php')
+      .addParam('u', 'http://crypt.ji/F937U2M9')
+      .bind()
+    Cryptoloji.UI.Sharer('twitter', '#share_button_tw')
+      .setShareLink('https://twitter.com/intent/tweet')
+      .addParam('url', 'http://crypt.ji/F937U2M9')
+      .addParam('text', 'a message')
+      .bind()
+    Cryptoloji.UI.Sharer('gplus', '#share_button_go')
+      .setShareLink('https://plus.google.com/share')
+      .addParam('url', 'http://crypt.ji/F937U2M9')
+      .bind()
+    // Cryptoloji.UI.Sharer('tumblr', '#share_button_tu')
+    //   .setShareLink('https://www.tumblr.com/widgets/share/tool')
+    //   .addParam('canonicalUrl', 'http://crypt.ji/F937U2M9')
+    //   .addParam('posttype', 'link')
+    //   .addParam('tags', 'mozilla,cryptoloji')
+    //   .addParam('description', 'a message')
+    //   .bind()
+    Cryptoloji.UI.Sharer('whatsapp', '#share_button_wa')
+      .sameWindow()
+      .setShareLink('whatsapp://send')
+      .addParam('text', 'http://crypt.ji/F937U2M9')
+      .bind()
+    Cryptoloji.UI.Sharer('mail', '#share_button_ma')
+      .sameWindow()
+      .setShareLink('mailto:')
+      .addParam('subject', 'A secret message')
+      .addParam('body', 'http://crypt.ji/F937U2M9')
+      .bind()
   }
 
 })(window, window.Cryptoloji); 
