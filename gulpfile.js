8de8cbfca6738f4f917836c7ad1dcb2860efe420
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-02-22 16:36:35 +0100

    favicon generation from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,37 @@ var minifyCss = require('gulp-minify-css');
 var del = require('del')
 var rev = require('gulp-rev')
 var minifyHtml = require('gulp-minify-html');
+var realFavicon = require('gulp-real-favicon');
+var fs = require('fs');
+var copy = require('gulp-copy');
+
+// File where the favicon markups are stored
+var FAVICON_DATA_FILE = 'faviconData.json';
+
+gulp.task('copy_fonts', ['clean'], function() {
+  return gulp.src('assets/font/**/*.{ttf,woff,woff2,eot,svg}')
+      .pipe(gulp.dest('public/font'));
+});
+
+gulp.task('copy_assets', ['clean'], function() {
+  var assets_paths = [
+    'assets/svg/**/*',
+    'assets/img/**/*',
+    'assets/js/touchmouse.js',
+    'vendor/**/*'
+  ]
+  
+  return gulp.src(assets_paths)
+             .pipe(copy('public'));
+});
 
 gulp.task('minify', ['clean'], function() {
-  gulp.src('index.html')
+  return gulp.src('index.html')
       .pipe(usemin({
-        css: [ minifyCss(), rev() ],
-        js: [ uglify(), rev() ],
-        vendorjs: [ uglify(), rev() ],
-        html: [ minifyHtml({ empty: true }) ],
+        css: [ minifyCss, rev ],
+        js: [ uglify, rev ],
+        vendorjs: [ uglify, rev ],
+        html: [ minifyHtml ],
       }))
       .pipe(gulp.dest('public'));
 });
@@ -26,4 +49,62 @@ gulp.task('clean', function() {
   ]);
 });
 
-gulp.task('build', ['clean', 'minify']);
+// Generate the icons. This task takes a few seconds to complete. 
+// You should run it at least once to create the icons. Then, 
+// you should run it whenever RealFaviconGenerator updates its 
+// package (see the check-for-favicon-update task below).
+gulp.task('generate-favicon', function(done) {
+  realFavicon.generateFavicon({
+    masterPicture: 'favicon.svg',
+    dest: 'public',
+    iconsPath: '/',
+    design: {
+      ios: {
+        pictureAspect: 'backgroundAndMargin',
+        backgroundColor: '#ffffff',
+        margin: '14%'
+      },
+      desktopBrowser: {},
+      windows: {
+        pictureAspect: 'noChange',
+        backgroundColor: '#da532c',
+        onConflict: 'override'
+      },
+      androidChrome: {
+        pictureAspect: 'shadow',
+        themeColor: '#ffffff',
+        manifest: {
+          name: 'Cryptoloji',
+          display: 'browser',
+          orientation: 'notSet',
+          onConflict: 'override',
+          declared: true
+        }
+      },
+      safariPinnedTab: {
+        pictureAspect: 'silhouette',
+        themeColor: '#5bbad5'
+      }
+    },
+    settings: {
+      compression: 2,
+      scalingAlgorithm: 'Mitchell',
+      errorOnImageTooSmall: false
+    },
+    markupFile: FAVICON_DATA_FILE
+  }, function() {
+    done();
+  });
+});
+
+// Inject the favicon markups in your HTML pages. You should run 
+// this task whenever you modify a page. You can keep this task 
+// as is or refactor your existing HTML pipeline.
+gulp.task('inject-favicon-markups', ['minify'], function() {
+  gulp.src('public/index.html')
+      .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+      .pipe(gulp.dest('public/'));
+});
+
+gulp.task('build', ['clean', 'copy_fonts', 'copy_assets', 'minify', 'inject-favicon-markups']);
+gulp.task('favicon', ['generate-favicon', 'inject-favicon-markups']);
