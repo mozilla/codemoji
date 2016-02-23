2f0e792f6d08b41d8e6065579146daf94bd32663
Amedeo Spagnolo <a.spagnolo@todo.to.it>
2016-02-23 12:40:52 +0100

    init logic views

--- assets/js/states/onboarding.js
+++ assets/js/states/onboarding.js
@@ -1,24 +1,40 @@
 (function (window, Cryptoloji, undefined) {
   'use strict'
-  
-  Cryptoloji.states.onboarding = {
+
+  Cryptoloji.states.onboarding = {}
+
+  Cryptoloji.states.onboarding.root = {
     enter: function () {
-
-      Cryptoloji.stateman.on('svg:loaded', function(p){
-        if(p == 'assets/svg/onboarding_bottom.svg'){
-          if(!Cryptoloji.onBoardingSlider) Cryptoloji.buildSlider('.onboarding_wrapper_ui')
-        }
-      })
-
-      if(!Cryptoloji.onBoardingSlider) Cryptoloji.buildSlider('.onboarding_wrapper_ui')
-      
-      $(".section_onboarding").addClass("section-show")
+      Cryptoloji.stateman.go('onboarding/step1')
+    }
+  }
+  
+  Cryptoloji.states.onboarding.step1 = {
+    enter: function () {
+      $(".section_onboarding_step1").addClass("section-show")
     },
     leave: function () {
-      $(".section_onboarding").removeClass("section-show")
+      $(".section_onboarding_step1").removeClass("section-show")
     }
   }
 
+  Cryptoloji.states.onboarding.step2 = {
+    enter: function () {
+      $(".section_onboarding_step2").addClass("section-show")
+    },
+    leave: function () {
+      $(".section_onboarding_step2").removeClass("section-show")
+    }
+  }
+
+  Cryptoloji.states.onboarding.step3 = {
+    enter: function () {
+      $(".section_onboarding_step3").addClass("section-show")
+    },
+    leave: function () {
+      $(".section_onboarding_step3").removeClass("section-show")
+    }
+  }
 })(window, window.Cryptoloji); 
 
 
