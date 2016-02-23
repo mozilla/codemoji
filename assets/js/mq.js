753b2761489226a9b810c996e509315c6d924b5a
Amedeo Spagnolo <a.spagnolo@todo.to.it>
2016-02-23 18:59:04 +0100

    fix media query matches in javascript

--- assets/js/mq.js
+++ assets/js/mq.js
@@ -1,5 +1,5 @@
 (function (window, Cryptoloji, undefined) {
 
-  Cryptoloji.mq = window.matchMedia("screen and (min-width: 1024px)").matches
+  Cryptoloji.mq = window.matchMedia("screen and (min-width: 1024px)")
 
 })(window, window.Cryptoloji); 
