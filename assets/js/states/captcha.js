42f5d4a98c153d512d102717e8522cdc0c09c340
marcoaimo <marcoaimo.am@gmail.com>
2016-02-23 15:36:24 +0100

    update captcha state view

--- assets/js/states/captcha.js
+++ assets/js/states/captcha.js
@@ -1,32 +1,47 @@
-(function (window, Cryptoloji, undefined) {
+(function (window, Cryptoloji, $, undefined) {
   'use strict'
-  
+
   console.log(Cryptoloji)
-  var captcha_url = "http://el.s.todo.to.it:3000"
+  // var grecaptcha = window.grecaptcha || null
+  var captcha_url = 'http://el.s.todo.to.it:3000'
+  var captchaId = -1
   Cryptoloji.states.captcha = {
+    canEnter: function () {
+      // prevent user from entering a broken share without message and key
+      // informations. Redirect to encrypt state
+      if (!Cryptoloji.current.key) {
+        Cryptoloji.stateman.go('encrypt')
+        return false
+      }
+      return true
+    },
     enter: function () {
       // fix height
-      $(".section_captcha").addClass("section-show")
-      $('#captcha_form').on('submit', function(event) {
-        event.preventDefault()
-        var captcha_string = { 'captcha_string' : $('#g-recaptcha-response').val() }
-
-        $.post(captcha_url + '/share', captcha_string)
-        .done(function() {
-          Cryptoloji.stateman.go('share')
+      if (captchaId < 0) {
+        captchaId = grecaptcha.render('g-recaptcha', {
+          'sitekey': '6Lc0txgTAAAAAPSW9_GalyD8WjVaDHONcbgH0oYe',
+          'callback': verifyCaptcha,
+          'theme': 'dark'
         })
-        .fail(function() {
-          alert('Retry!')
-        })
-      })
+      } else {
+        grecaptcha.reset()
+      }
+      $('.section_captcha').addClass('section-show')
     },
     leave: function () {
-      $(".section_captcha").removeClass("section-show")
-      $('#captcha_form').off('submit')
-      grecaptcha.reset();
+      $('.section_captcha').removeClass('section-show')
     }
   }
 
+  function verifyCaptcha () {
+    var captcha_string = {'captcha_string': $('#g-recaptcha-response').val()}
+    $.post(captcha_url + '/share', captcha_string)
+    .done(function () {
+      Cryptoloji.stateman.go('share')
+    })
+    .fail(function () {
+      alert('Retry!')
+    })
+  }
 
-
-})(window, window.Cryptoloji); 
+})(window, window.Cryptoloji, window.jQuery)
