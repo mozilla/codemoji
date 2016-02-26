250ad1b19dc3565a48659f3cb7eb7f082c153afb
Amedeo Spagnolo <a.spagnolo@todo.to.it>
2016-02-26 19:16:38 +0100

    animation reset encrypt

--- assets/js/states/encrypt.js
+++ assets/js/states/encrypt.js
@@ -9,19 +9,19 @@
       Cryptoloji.stateman.emit('footer:show') 
 
       // draft logic transition 
-      // if(Cryptoloji.mq.matches){
-      //   TweenLite.to($('.encryption .main_keyslider'), 0, {delay: 0, width: "0%"})
-      //   TweenLite.to($('.encryption .main_keyslider'), .5, {delay: 3, width: "35%"})
+      if(Cryptoloji.mq.matches){
+        TweenLite.to($('.encryption .main_keyslider'), 0, {delay: 0, width: "0%"})
+        TweenLite.to($('.encryption .main_keyslider'), .5, {delay: 3, width: "35%"})
 
-      //   TweenLite.to($('.encryption .main_keyslider .main_key_panel_emoji_wrapper, .encryption .main_keyslider .main_keyslider_bottom_label'), 0, {delay: 0, opacity: "0"})
-      //   TweenLite.to($('.encryption .main_keyslider .main_key_panel_emoji_wrapper, .encryption .main_keyslider .main_keyslider_bottom_label'), .5, {delay: 4, opacity: "1"})
+        TweenLite.to($('.encryption .main_keyslider .main_key_panel_emoji_wrapper, .encryption .main_keyslider .main_keyslider_bottom_label'), 0, {delay: 0, opacity: "0"})
+        TweenLite.to($('.encryption .main_keyslider .main_key_panel_emoji_wrapper, .encryption .main_keyslider .main_keyslider_bottom_label'), .5, {delay: 4, opacity: "1"})
         
-      //   TweenLite.to($('.main_content_bottom_input.placeholdit'), 0, {delay: 0, opacity: "0"})
-      //   TweenLite.to($('.main_content_bottom_input.placeholdit'), .5, {delay: 4, opacity: ".5"})
+        TweenLite.to($('.main_content_bottom_input.placeholdit'), 0, {delay: 0, opacity: "0"})
+        TweenLite.to($('.main_content_bottom_input.placeholdit'), .5, {delay: 4, opacity: ".5"})
 
-      //   TweenLite.to($('.main_content_bottom_label'), 0, {delay: 0, opacity: "0"})
-      //   TweenLite.to($('.main_content_bottom_label'), .5, {delay: 4, opacity: "1"})
-      // }
+        TweenLite.to($('.main_content_bottom_label'), 0, {delay: 0, opacity: "0"})
+        TweenLite.to($('.main_content_bottom_label'), .5, {delay: 4, opacity: "1"})
+      }
 
       Cryptoloji.UI.CharCounter('encrypt', '#encryption_input_count')
         .setMaxSize(Cryptoloji.settings.inputMaxSize)
