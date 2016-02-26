1d1e05b1ad3eaccfd6c930f0c60d20dbd54b5295
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-02-26 15:41:10 +0100

    404 page first draft

---
+++ assets/js/states/404.js
@@ -0,0 +1,10 @@
+(function (window, Cryptoloji, $, undefined) {
+  'use strict'
+
+  Cryptoloji.states.notFound = {
+    enter: function () {
+      $('.section_not_found').addClass('section-show')
+    }
+  }
+
+})(window, window.Cryptoloji, window.jQuery);
