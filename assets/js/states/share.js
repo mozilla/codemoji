672d941439af6b9c39c829a462ca1390fed88f58
marcoaimo <marcoaimo.am@gmail.com>
2016-02-29 17:19:46 +0100

    remove placeholder text and key and link to real ones

--- assets/js/states/share.js
+++ assets/js/states/share.js
@@ -32,14 +32,14 @@
         $('.share_key').css('position', 'absolute')
         $('.share_key').css('height', '50%')
         $('.share_key').css('background-color', '#F46060')
-        $('#share_currentkey').css('border', 'none')
+        $('.share_key_emoji-item').css('border', 'none')
         $('.share_key .hidden').css('display', 'block')
       })
       $('#share_key_hide').on('click', function () {
         $('.share_key').css('position', 'relative')
         $('.share_key').css('height', 'auto')
         $('.share_key').css('background-color', '')
-        $('#share_currentkey').css('border', '.5rem solid')
+        $('.share_key_emoji-item').css('border', '.5rem solid')
         $('.share_key .hidden').css('display', 'none')
       })
     },
