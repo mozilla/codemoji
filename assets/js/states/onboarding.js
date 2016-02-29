da3431fb7fd418bfc03bd788beec09862f996c86
terminalia <l.zanconi@todo.to.it>
2016-02-29 18:08:02 +0100

    Add new onboarding animation slide 1

--- assets/js/states/onboarding.js
+++ assets/js/states/onboarding.js
@@ -17,6 +17,14 @@
         $('[slide-num="'+n+'"]').on("click", function(){
           Cryptoloji.stateman.go('onboarding.step'+(n+1))
         })
+
+        if (n === 1) {
+          Cryptoloji.stateman.on('svg:loaded', function(path) {
+            if (path === "assets/svg/slide01.svg") {
+              animate_onboarding1()
+            }
+          })
+        }
       },
       leave: function () {
         $('[slide-num="'+n+'"]').removeClass("section-show")
@@ -36,6 +44,56 @@
         Cryptoloji.stateman.emit('header:hide')
       }
     }
+  }
+
+  function animate_onboarding1() {
+    var tl = new TimelineLite();
+    //Set initial state
+    tl.to($("#onboarding_slide_1_A1"), 0.0, {x: -200, y: -200.0})
+    .to($("#onboarding_slide_1_A2"), 0.0, {x: -200, y: -200.0})
+    .to($("#onboarding_slide_1_A3"), 0.0, {x: -200, y: -200.0})
+    .to($("#onboarding_slide_1_A4"), 0.0, {x: -200, y: -200.0})
+    .to($("#onboarding_slide_1_A5"), 0.0, {x: -200, y: -200.0})
+    .to($("#onboarding_slide_1_A2"), 0.0, {x: -200, y: -200.0})
+    .to($("#onboarding_slide_1_encrypted_B1"), 0.0, {y: -205})
+    .to($("#onboarding_slide_1_encrypted_B2"), 0.0, {y: -205})
+    .to($("#onboarding_slide_1_encrypted_B3"), 0.0, {y: -205})
+    .to($("#onboarding_slide_1_encrypted_B4"), 0.0, {y: -205})
+    .to($("#onboarding_slide_1_encrypted_B5"), 0.0, {y: -205})
+    .to($("#onboarding_slide1_line_1"), 0.0, {y: -205})
+    .to($("#onboarding_slide1_line_2"), 0.0, {y: -205})
+    .to($("#onboarding_slide1_line_3"), 0.0, {y: -205})
+    .to($("#onboarding_slide1_line_4"), 0.0, {y: -205})
+    .to($("#onboarding_slide1_line_5"), 0.0, {y: -205})
+    .to($("#onboarding_slide_1_text"), 0.0, {opacity: 0.0})
+
+    //Animation
+    tl.to($("#onboarding_slide_1_A1"), 0.25, {x: 0.0, y: 0.0})
+    .to($("#onboarding_slide_1_A2"), 0.25, {x: 0.0, y: 0.0})
+    .to($("#onboarding_slide_1_A4"), 0.25, {x: 0.0, y: 0.0})
+    .to($("#onboarding_slide_1_A3"), 0.25, {x: 0.0, y: 0.0})
+    .to($("#onboarding_slide_1_A5"), 0.25, {x: 0.0, y: 0.0})
+    .to($("#onboarding_slide_1_A1"), 0.1, {rotation: 360, transformOrigin: "50% 50%"})
+    .to($("#onboarding_slide_1_A1"), 0.1, {rotation: -360, transformOrigin: "50% 50%"})
+    .to($("#onboarding_slide1_line_5"), 0.1, {y: 0.0})
+    .to($("#onboarding_slide_1_encrypted_B1"), 0.1, {y: 0.0})
+    .to($("#onboarding_slide_1_A2"), 0.1, {rotation: 360, transformOrigin: "50% 50%"})
+    .to($("#onboarding_slide_1_A2"), 0.1, {rotation: -360, transformOrigin: "50% 50%"})
+    .to($("#onboarding_slide1_line_4"), 0.1, {y: 0.0})
+    .to($("#onboarding_slide_1_encrypted_B2"), 0.1, {y: 0.0})
+    .to($("#onboarding_slide_1_A4"), 0.1, {rotation: 360, transformOrigin: "50% 50%"})
+    .to($("#onboarding_slide_1_A4"), 0.1, {rotation: -360, transformOrigin: "50% 50%"})
+    .to($("#onboarding_slide1_line_3"), 0.1, {y: 0.0})
+    .to($("#onboarding_slide_1_encrypted_B3"), 0.1, {y: 0.0})
+    .to($("#onboarding_slide_1_A3"), 0.1, {rotation: 360, transformOrigin: "50% 50%"})
+    .to($("#onboarding_slide_1_A3"), 0.1, {rotation: -360, transformOrigin: "50% 50%"})
+    .to($("#onboarding_slide1_line_2"), 0.1, {y: 0.0})
+    .to($("#onboarding_slide_1_encrypted_B4"), 0.1, {y: 0.0})
+    .to($("#onboarding_slide_1_A5"), 0.1, {rotation: 360, transformOrigin: "50% 50%"})
+    .to($("#onboarding_slide_1_A5"), 0.1, {rotation: -360, transformOrigin: "50% 50%"})
+    .to($("#onboarding_slide1_line_1"), 0.1, {y: 0.0})
+    .to($("#onboarding_slide_1_encrypted_B5"), 0.1, {y: 0.0})
+    .to($("#onboarding_slide_1_text"), 1.0, {opacity: 1.0})
   }
 
   Cryptoloji.states.onboarding.step1 = next_slide(1)
