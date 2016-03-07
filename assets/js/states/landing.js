7b7adeb29c8d3f757c16537b95807e605a274f89
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-03-07 13:35:25 +0100

    fill message in landing view

--- assets/js/states/landing.js
+++ assets/js/states/landing.js
@@ -3,6 +3,7 @@
   
   Cryptoloji.states.landing = {
     enter: function () {
+      fillMessage()
 
       $(".section_landing").addClass("section-show")
       Cryptoloji.stateman.emit('footer:show') 
@@ -11,6 +12,11 @@
       $(".section_landing").removeClass("section-show")
       Cryptoloji.stateman.emit('footer:hide') 
     }
+  }
+
+  function fillMessage () {
+    var message = Cryptoloji.storage.get('message')
+    $('#landing_encrypted_message').html(Cryptoloji.UI.toTwemoji(message))
   }
 
 })(window, window.Cryptoloji); 
