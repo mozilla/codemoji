77ed6de49fa974d9cec4e70b43f2981d0643cdb7
terminalia <l.zanconi@todo.to.it>
2016-03-07 12:14:21 +0100

    Update onboarding animation

--- assets/js/states/onboarding.js
+++ assets/js/states/onboarding.js
@@ -29,13 +29,28 @@
               var svg_elements = ["#onboarding_slide_2_encrypted_hello>g", "#onboarding_slide_2_encrypted_hello_2>g", "#onboarding_slide_2_encrypted_hello_3>g", "#onboarding_slide_2_encrypted_hello_4>g"]
               Cryptoloji.UI.animate_onboarding(svg_elements)
           break;
+
+        case 3:
+          animate_slide_3()
+          break;
+          
+        case 4:
+          animate_slide_4()
+          break;
+
+        case 5:
+          animate_slide_5()
+          break; 
+
+        case 6:
+          animate_slide_6()
+          break;
       }
   }
 
   function next_slide(n){
     return {
       enter: function () {
-
         // display cross-slide elements
         $('.section_onboarding_wrapper').addClass("section-show")
 
@@ -59,103 +74,6 @@
             slideLogic (n)
           })
         }
-
-
-        if (n === 3) {
-          $('#slide_3_plain_text > path').each(function(i) {
-            TweenLite.set($('#slide_3_plain_text > path')[i], {opacity: 0.0})
-          })
-          TweenLite.set($('#slide_3_plain_text_bg'), {y: -300})
-
-          
-          var text_animation_duration = 0.1
-          var bg_animation_duration = 1.0
-          TweenLite.to($("#slide_3_plain_text_bg"), 1.0, {y: 0})
-          TweenLite.to($("#slide_3_plain_text > path")[0], text_animation_duration, {delay: bg_animation_duration, opacity: 1.0})
-          TweenLite.to($("#slide_3_plain_text > path")[1], text_animation_duration, {delay: bg_animation_duration + 0.1, opacity: 1.0})
-          TweenLite.to($("#slide_3_plain_text > path")[2], text_animation_duration, {delay: bg_animation_duration + 0.2, opacity: 1.0})
-          TweenLite.to($("#slide_3_plain_text > path")[3], text_animation_duration, {delay: bg_animation_duration + 0.3, opacity: 1.0})
-          TweenLite.to($("#slide_3_plain_text > path")[4], text_animation_duration, {delay: bg_animation_duration + 0.4, opacity: 1.0})
-          TweenLite.to($("#slide_3_plain_text > path")[5], text_animation_duration, {delay: bg_animation_duration + 0.5, opacity: 1.0})
-          TweenLite.to($("#slide_3_plain_text > path")[6], text_animation_duration, {delay: bg_animation_duration + 0.6, opacity: 1.0})
-          TweenLite.to($("#slide_3_plain_text > path")[7], text_animation_duration, {delay: bg_animation_duration + 0.7, opacity: 1.0})
-          TweenLite.to($("#slide_3_plain_text > path")[8], text_animation_duration, {delay: bg_animation_duration + 0.8, opacity: 1.0})
-          TweenLite.to($("#slide_3_plain_text > path")[9], text_animation_duration, {delay: bg_animation_duration + 0.9, opacity: 1.0})
-          TweenLite.to($("#slide_3_plain_text > path")[10], text_animation_duration, {delay: bg_animation_duration + 1.0, opacity: 1.0})
-          TweenLite.to($("#slide_3_plain_text > path")[11], text_animation_duration, {delay: bg_animation_duration + 1.1, opacity: 1.0})
-
-        }
-
-        if (n === 4) {
-          timeline.clear()
-
-          $('#slide_4_plain_text > path').each(function(i) { 
-            TweenLite.set($('#slide_4_plain_text > path')[i], {opacity: 1.0})
-          })
-          $('#slide_4_encrypted_emoji > g').each(function(i) { 
-            TweenLite.set($('#slide_4_encrypted_emoji > g')[i], {opacity: 0.0})
-          })
-          TweenLite.set($('#slide_4_emoji_selector'), {opacity: 0})
-
-          TweenLite.set($("#slide_4_emoji_1"), {opacity: 0})
-          TweenLite.set($("#slide_4_emoji_2"), {opacity: 0})
-          TweenLite.set($("#slide_4_emoji_3"), {opacity: 0})
-
-          TweenLite.to($("#slide_4_emoji_1"), 0.5, {delay: 1, opacity: 1})
-          TweenLite.to($("#slide_4_emoji_2"), 0.5, {delay: 1, opacity: 1})
-          TweenLite.to($("#slide_4_emoji_3"), 0.5, {delay: 1, opacity: 1, onComplete: function() { timeline.play()}})
-
-          timeline.to($('#slide_4_emoji_2'), 0.3, {delay: 1.5, opacity: 0.2})
-          .to($('#slide_4_emoji_2'), 0.3, {opacity: 1.0})
-          .to($('#slide_4_emoji_2'), 0.3, {opacity: 0.2})
-          .to($('#slide_4_emoji_2'), 0.3, {opacity: 1.0})
-          .to($('#slide_4_emoji_2'), 0.3, {opacity: 0.2})
-          .to($('#slide_4_emoji_2'), 0.3, {opacity: 1.0})
-          .to($('#slide_4_emoji_selector'), 0.4, {opacity: 1.0})
-          .to($('#slide_4_emoji_selector'), 0.5, {x: -105})
-          .to($('#slide_4_emoji_selector'), 0.0, {delay: 1, x: 105})
-          .to($('#slide_4_emoji_selector'), 0.5, {delay: 1, x: 0, onComplete: function() {
-            var text_animation_duration = 0.1
-            $('#slide_4_plain_text > path').each(function(i) {
-              TweenLite.to($('#slide_4_plain_text > path')[i], text_animation_duration, {delay: i * text_animation_duration, opacity: 0.0})
-            })
-
-            $('#slide_4_encrypted_emoji > g').each(function(i) { 
-              TweenLite.to($(this), text_animation_duration, {delay: i * text_animation_duration, opacity: 1.0})
-            })
-          }})
-        }
-
-        if (n === 5) {
-          timeline.clear()
-          timeline.set($('#slide_5_delivered_text'), {y: 100, opacity: 0})
-          timeline.set($('#slide_5_bubble_white_text_bg'), {y: 100, opacity: 0})
-          timeline.set($('#slide_5_bubble_white_text'), {opacity: 0})
-          timeline.set($('#slide_5_bubble_white_dots_bg'), {opacity: 0})
-          timeline.set($('#slide_5_bubble_white_dots > circle')[0], {opacity: 0})
-          timeline.set($('#slide_5_bubble_white_dots > circle')[1], {opacity: 0})
-          timeline.set($('#slide_5_bubble_white_dots > circle')[2], {opacity: 0})
-
-          timeline.to($('#slide_5_delivered_text'), 0.5, {delay: 0.5, y: 0, opacity: 1})
-          .to($('#slide_5_bubble_white_dots_bg'), 0.5, {delay: 0.5, y: 0, opacity: 1})
-          .to($('#slide_5_bubble_white_dots > circle')[0], 0.0, {opacity: 1})
-          .to($('#slide_5_bubble_white_dots > circle')[1], 0.0, {opacity: 1})
-          .to($('#slide_5_bubble_white_dots > circle')[2], 0.0, {opacity: 1})
-          .to($('#slide_5_bubble_white_dots > circle')[0], 0.05, {y: -5})
-          .to($('#slide_5_bubble_white_dots > circle')[1], 0.05, {y: -5})
-          .to($('#slide_5_bubble_white_dots > circle')[2], 0.05, {y: -5})
-          .to($('#slide_5_bubble_white_dots > circle')[0], 0.05, {y: 0})
-          .to($('#slide_5_bubble_white_dots > circle')[1], 0.05, {y: 0})
-          .to($('#slide_5_bubble_white_dots > circle')[2], 0.05, {y: 0})
-          .to($('#slide_5_bubble_white_dots > circle')[0], 0.05, {y: -5})
-          .to($('#slide_5_bubble_white_dots > circle')[1], 0.05, {y: -5})
-          .to($('#slide_5_bubble_white_dots > circle')[2], 0.05, {y: -5})
-          .to($('#slide_5_bubble_white_dots > circle')[0], 0.05, {y: 0})
-          .to($('#slide_5_bubble_white_dots > circle')[1], 0.05, {y: 0})
-          .to($('#slide_5_bubble_white_dots > circle')[2], 0.05, {y: 0})
-
-
-        }
       },
       leave: function () {
         $('[slide-num="'+n+'"]').removeClass("section-show")
@@ -178,6 +96,109 @@
         Cryptoloji.stateman.emit('header:hide')
       }
     }
+  }
+  
+  function animate_slide_3() {
+    $('#slide_3_plain_text > path').each(function(i) {
+            TweenLite.set($(this), {opacity: 0.0})
+    })
+    TweenLite.set($('#slide_3_plain_text_bg'), {y: -300})
+
+    
+    var text_animation_duration = 0.1
+    var bg_animation_duration = 1.0
+    TweenLite.to($("#slide_3_plain_text_bg"), 1.0, {y: 0})
+    TweenLite.to($("#slide_3_plain_text > path")[0], text_animation_duration, {delay: bg_animation_duration, opacity: 1.0})
+    TweenLite.to($("#slide_3_plain_text > path")[1], text_animation_duration, {delay: bg_animation_duration + 0.1, opacity: 1.0})
+    TweenLite.to($("#slide_3_plain_text > path")[2], text_animation_duration, {delay: bg_animation_duration + 0.2, opacity: 1.0})
+    TweenLite.to($("#slide_3_plain_text > path")[3], text_animation_duration, {delay: bg_animation_duration + 0.3, opacity: 1.0})
+    TweenLite.to($("#slide_3_plain_text > path")[4], text_animation_duration, {delay: bg_animation_duration + 0.4, opacity: 1.0})
+    TweenLite.to($("#slide_3_plain_text > path")[5], text_animation_duration, {delay: bg_animation_duration + 0.5, opacity: 1.0})
+    TweenLite.to($("#slide_3_plain_text > path")[6], text_animation_duration, {delay: bg_animation_duration + 0.6, opacity: 1.0})
+    TweenLite.to($("#slide_3_plain_text > path")[7], text_animation_duration, {delay: bg_animation_duration + 0.7, opacity: 1.0})
+    TweenLite.to($("#slide_3_plain_text > path")[8], text_animation_duration, {delay: bg_animation_duration + 0.8, opacity: 1.0})
+    TweenLite.to($("#slide_3_plain_text > path")[9], text_animation_duration, {delay: bg_animation_duration + 0.9, opacity: 1.0})
+    TweenLite.to($("#slide_3_plain_text > path")[10], text_animation_duration, {delay: bg_animation_duration + 1.0, opacity: 1.0})
+    TweenLite.to($("#slide_3_plain_text > path")[11], text_animation_duration, {delay: bg_animation_duration + 1.1, opacity: 1.0})
+  }
+
+  function animate_slide_4() {
+    timeline.clear()
+    $('#slide_4_plain_text > path').each(function(i) { 
+      TweenLite.set($(this), {opacity: 1.0})
+    })
+    $('#slide_4_encrypted_emoji > g').each(function(i) { 
+      TweenLite.set($(this), {opacity: 0.0})
+    })
+    TweenLite.set($('#slide_4_emoji_selector'), {opacity: 0})
+
+    TweenLite.set($("#slide_4_emoji_1"), {opacity: 0})
+    TweenLite.set($("#slide_4_emoji_2"), {opacity: 0})
+    TweenLite.set($("#slide_4_emoji_3"), {opacity: 0})
+
+    TweenLite.to($("#slide_4_emoji_1"), 0.5, {delay: 1, opacity: 1})
+    TweenLite.to($("#slide_4_emoji_2"), 0.5, {delay: 1, opacity: 1})
+    TweenLite.to($("#slide_4_emoji_3"), 0.5, {delay: 1, opacity: 1, onComplete: function() { timeline.play()}})
+
+    timeline.to($('#slide_4_emoji_2'), 0.3, {delay: 1.5, opacity: 0.2})
+    .to($('#slide_4_emoji_2'), 0.3, {opacity: 1.0})
+    .to($('#slide_4_emoji_2'), 0.3, {opacity: 0.2})
+    .to($('#slide_4_emoji_2'), 0.3, {opacity: 1.0})
+    .to($('#slide_4_emoji_2'), 0.3, {opacity: 0.2})
+    .to($('#slide_4_emoji_2'), 0.3, {opacity: 1.0})
+    .to($('#slide_4_emoji_selector'), 0.4, {opacity: 1.0})
+    .to($('#slide_4_emoji_selector'), 0.5, {x: -105})
+    .to($('#slide_4_emoji_selector'), 0.0, {delay: 1, x: 105})
+    .to($('#slide_4_emoji_selector'), 0.5, {delay: 1, x: 0, onComplete: function() {
+      var text_animation_duration = 0.1
+      $('#slide_4_plain_text > path').each(function(i) {
+        TweenLite.to($(this), text_animation_duration, {delay: i * text_animation_duration, opacity: 0.0})
+      })
+
+      $('#slide_4_encrypted_emoji > g').each(function(i) { 
+        TweenLite.to($(this), text_animation_duration, {delay: i * text_animation_duration, opacity: 1.0})
+      })
+    }})
+  }
+
+  function animate_slide_5() {
+    timeline.clear()
+    timeline.set($('#slide_5_delivered_text'), {y: 100, opacity: 0})
+    timeline.set($('#slide_5_bubble_white_text_bg'), {y: 80, opacity: 0})
+    timeline.set($('#slide_5_bubble_white_text'), {opacity: 0})
+    timeline.set($('#slide_5_bubble_white_dots_bg'), {opacity: 0})
+    timeline.set($('#slide_5_bubble_white_dots'), {opacity: 1})
+
+    .to($('#slide_5_delivered_text'), 0.5, {delay: 0.5, y: 0, opacity: 1})
+    .to($('#slide_5_bubble_white_dots_bg'), 0.5, {delay: 0.5, y: 0, opacity: 1})
+    .to($('#slide_5_bubble_white_dots > circle')[0], 0.0, {opacity: 1})
+    .to($('#slide_5_bubble_white_dots > circle')[1], 0.0, {opacity: 1})
+    .to($('#slide_5_bubble_white_dots > circle')[2], 0.0, {opacity: 1})
+    .to($('#slide_5_bubble_white_dots > circle')[0], 0.05, {y: -5})
+    .to($('#slide_5_bubble_white_dots > circle')[1], 0.05, {y: -5})
+    .to($('#slide_5_bubble_white_dots > circle')[2], 0.05, {y: -5})
+    .to($('#slide_5_bubble_white_dots > circle')[0], 0.05, {y: 0})
+    .to($('#slide_5_bubble_white_dots > circle')[1], 0.05, {y: 0})
+    .to($('#slide_5_bubble_white_dots > circle')[2], 0.05, {y: 0})
+    .to($('#slide_5_bubble_white_dots > circle')[0], 0.05, {y: -5})
+    .to($('#slide_5_bubble_white_dots > circle')[1], 0.05, {y: -5})
+    .to($('#slide_5_bubble_white_dots > circle')[2], 0.05, {y: -5})
+    .to($('#slide_5_bubble_white_dots > circle')[0], 0.05, {y: 0})
+    .to($('#slide_5_bubble_white_dots > circle')[1], 0.05, {y: 0})
+    .to($('#slide_5_bubble_white_dots > circle')[2], 0.05, {y: 0})
+    .to($('#slide_5_bubble_white_dots_bg'), 0.1, {opacity: 0})
+    .to($('#slide_5_bubble_white_dots'), 0.1, {y: 0, opacity: 0})
+    .to($('#slide_5_bubble_white_text_bg'), 0.5, {y: 0, opacity: 1})
+    .to($('#slide_5_bubble_white_text'), 0.5, {opacity: 1})
+  }
+
+  function animate_slide_6() {
+    timeline.clear()
+    timeline.set($('#slide_6_delivered_text'), {y: 100, opacity: 0})
+    timeline.set($('#slide_6_bubble_white_dots_bg'), {opacity: 0})
+    timeline.set($('#slide_6_bubble_white_dots'), {opacity: 0})
+
+    timeline.to($('#slide_6_emoji_bg'), 0.5, {y: -200})
   }
 
   Cryptoloji.states.onboarding.step1 = next_slide(1)
