700e0222d385f0e712873efa63dd9db5c2f4227b
marcoaimo <marcoaimo.am@gmail.com>
2016-03-09 15:28:36 +0100

    handle svg loader hold document ready till all svgs are loaded

--- assets/js/handleSvgLoading.js
+++ assets/js/handleSvgLoading.js
@@ -1,19 +1,23 @@
 function gHandleSvgLoading (svg_loaded) {
-    function loadSvg (element, path) {
-        $.get(path)
-         .done(function (result) {
-            $(element).append(result.documentElement)
-            svg_loaded.push(path)
-            Cryptoloji.stateman.emit('svg:loaded', path)
-          })
-         .fail(function () {
-            console.error(this)
-          })
-    }
-    var svgsPromises = []
-    $("div[data-svg*='assets/svg']").each(function () {
-      var self = this
-      var path = $(self).attr('data-svg')
-      loadSvg(self, path)
-    })
+  function loadSvg (element, path) {
+      return $.get(path)
+       .done(function (result) {
+          $(element).append(result.documentElement)
+          svg_loaded.push(path)
+        })
+       .fail(function () {
+          console.error(this)
+        })
   }
+  $.holdReady(true)
+  var svgsPromises = []
+  $("div[data-svg*='assets/svg']").each(function () {
+    var self = this
+    var path = $(self).attr('data-svg')
+    svgsPromises.push(loadSvg(self, path))
+  })
+  $.when.apply($, svgsPromises).then(function(){
+    console.log('all svg loaded!')
+    $.holdReady(false)
+  })
+}
