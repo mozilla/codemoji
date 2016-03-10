538f900520411fab04dd4782cae07d09d6379654
abusedmedia <ff@todo.to.it>
2016-03-10 18:01:15 +0100

    restored url plain

--- assets/js/api.js
+++ assets/js/api.js
@@ -14,7 +14,8 @@
 
     // shareURI should be sent to bit.ly to be shortened
 
-    deferred.resolve('http://bit.ly/1M8Iuj1') // should be shortened shareURI
+    //deferred.resolve('http://bit.ly/1M8Iuj1') // should be shortened shareURI
+    deferred.resolve(shareURI)
 
     return deferred.promise()
   }
