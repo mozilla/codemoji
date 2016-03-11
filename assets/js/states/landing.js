84c6372d43042c5a92478ec7f00c785a44413123
Amedeo Spagnolo <a.spagnolo@todo.to.it>
2016-03-11 18:20:15 +0100

    fix dimesion text landing message

--- assets/js/states/landing.js
+++ assets/js/states/landing.js
@@ -11,6 +11,7 @@
       }
     },
     enter: function () {
+
       fillMessage()
 
       $(".section_landing").addClass("section-show")
@@ -52,6 +53,30 @@
   function fillMessage () {
     var message = Cryptoloji.storage.get('message')
     $('#landing_state_1_encrypted_message').html(Cryptoloji.UI.toTwemoji(message))
+    var emojiSize = $('body').height() / 480 * 100
+    var emojiLength = 120
+    if (message) {
+      emojiLength = message.length
+    }
+    var molt = 1/2
+    if (emojiLength == 1) {
+      emojiSize = emojiSize * molt * 12 
+    } else if (emojiLength >= 2 && emojiLength < 13) {
+      emojiSize = emojiSize * molt * 4 
+    } else if (emojiLength >= 13 && emojiLength < 20) {
+      emojiSize = emojiSize * molt * 3 
+    } else if (emojiLength >= 20 && emojiLength < 36) {
+      emojiSize = emojiSize * molt * 2 
+    } else if (emojiLength >= 36 && emojiLength < 60) {
+      emojiSize = emojiSize * molt * 1.7 
+    } else if (emojiLength >= 60 && emojiLength < 80) {
+      emojiSize = emojiSize * molt * 1.5 
+    } else if (emojiLength >= 80 && emojiLength < 100) {
+      emojiSize = emojiSize * molt * 1.2 
+    } else {
+      emojiSize = emojiSize * molt * 1 
+    }
+    $('#landing_state_1_encrypted_message').css('font-size', emojiSize + '%')
   }
 
 })(window, window.Cryptoloji); 
