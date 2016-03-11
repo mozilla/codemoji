6ad5ebd1597a4dc271843ead45be0ff8515235fd
Amedeo Spagnolo <a.spagnolo@todo.to.it>
2016-03-11 17:03:23 +0100

    fix animation landing decrypt

--- assets/js/states/landing.js
+++ assets/js/states/landing.js
@@ -16,12 +16,30 @@
       $(".section_landing").addClass("section-show")
       Cryptoloji.stateman.emit('footer:show') 
 
+      var time = .3
+      var delay = .4
       TweenLite.set($("[landing-state]"), {display: "none"})
-      TweenLite.set($("[landing-state='1']"), {display: "block"})
+      TweenLite.set($("[landing-state='1']"), {display: "block", opacity: 0})
+      // text
+      TweenLite.to($("#landing_state_1_text"), time, {opacity: 1, delay: delay})
+      // svg
+      TweenLite.to($("#landing_state_1_encrypted_message"), time, {opacity: 1, delay: delay * 2})
+      // button
+      TweenLite.to($("#landing_state_1_button"), time, {opacity: 1, delay: delay * 3})
 
-      $("#landing_button_first").on("click", function(){
+
+      $("#landing_state_1_button").on("click", function(){
         TweenLite.set($("[landing-state]"), {display: "none"})
-        TweenLite.set($("[landing-state='2']"), {display: "block"})
+        TweenLite.set($("[landing-state='2']"), {display: "block", opacity: 0})
+        TweenLite.set($("#landing_state_2_question_mark"), {opacity: 0})
+        // text
+        TweenLite.to($("#landing_state_2_text"), time, {opacity: 1, delay: delay})
+        // svg
+        TweenLite.to($("#landing_state_2_svg"), time, {opacity: 1, delay: delay * 2})
+        TweenLite.to($("#landing_state_2_emoji"), time, {opacity: .3, delay: delay * 3})
+        TweenLite.to($("#landing_state_2_question_mark"), time, {opacity: 1, delay: delay * 3})
+        // button
+        TweenLite.to($("#landing_state_2_button"), time, {opacity: 1, delay: delay * 3})
       })
 
     },
@@ -33,7 +51,7 @@
 
   function fillMessage () {
     var message = Cryptoloji.storage.get('message')
-    $('#landing_encrypted_message').html(Cryptoloji.UI.toTwemoji(message))
+    $('#landing_state_1_encrypted_message').html(Cryptoloji.UI.toTwemoji(message))
   }
 
 })(window, window.Cryptoloji); 
