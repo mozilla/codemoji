1873faf06576d487b52b22874be3a610aa51a7b0
terminalia <l.zanconi@todo.to.it>
2016-03-11 10:43:37 +0100

    Fix Slide 3 fade in tutor text timing

--- assets/js/states/onboarding.js
+++ assets/js/states/onboarding.js
@@ -163,7 +163,7 @@
       .set('#onboarding_slide_3_text', { opacity: 0 })
       .to('#onboarding_slide_3_plain_text_bg', 0.5, { ease: Bounce.easeOut, y: 0 })
       .staggerTo('#onboarding_slide_3_plain_text > g', .05, { opacity: 1.0 }, .1)
-      .to('#onboarding_slide_3_text', 0.1, { delay: 0.3, opacity: 1 })
+      .to('#onboarding_slide_3_text', 0.5, { delay: 0.3, opacity: 1 })
       .set('#next_button_onboarding', { display: 'block' })
       .to('#next_button_onboarding', 0.1, { delay: 0.3, scale: 1, opacity: 1 })
 
