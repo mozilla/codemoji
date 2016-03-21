ab0bd6a27b8c079f1cbea61c72fe7e11fd82d09b
marcoaimo <marcoaimo.am@gmail.com>
2016-03-21 18:14:03 +0100

    minor fix

--- assets/js/states/encrypt.js
+++ assets/js/states/encrypt.js
@@ -14,7 +14,7 @@
         setTimeout(function(){
               $('#encryptHeader').transition({y:0, duration:1000, delay:250, easing:'easeInOutExpo'})
         },0)
-      } else {
+      } else if(Cryptoloji.mq.matches) {
         $('.encryption_help_button').show()
         TweenLite.to($('.encryption_help_button'), 1.25, {opacity: 1, ease:Elastic.easeInOut})
       }
