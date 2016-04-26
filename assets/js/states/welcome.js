2d82e40c8ec878d5f3e0b7a21dc0a2a909f7a589
Amedeo Spagnolo <a.spagnolo@todo.to.it>
2016-04-26 15:56:48 +0200

    logo animation draft

--- assets/js/states/welcome.js
+++ assets/js/states/welcome.js
@@ -1,6 +1,45 @@
 (function (window, Cryptoloji, undefined) {
   'use strict'
-  
+    
+  function triggerLogoAnimation () {
+    TweenLite.set($("#final_emoji_1"),{opacity: 0})
+    TweenLite.set($("#final_emoji_2"),{opacity: 0})
+
+    TweenLite.set($("#animation1"),{display: 'block'})
+    TweenLite.set($("#animation2"),{display: 'block'})
+    TweenLite.set($("#animation1 > g"),{opacity: 0})
+    TweenLite.set($("#animation2 > g"),{opacity: 0})
+
+    $("#animation1 > g").each(function(d){
+        popEmoji(this,d)
+    })
+    $("#animation2 > g").each(function(d){
+        popEmoji(this,d)
+    })
+
+    function popEmoji(el,i) {
+      var speed = .8
+      var general_delay = 1.5
+      TweenLite.from(el, speed/4, {
+        scale: 2, 
+        transformOrigin: 'center center',
+        delay: i * speed + general_delay + speed/4
+      })
+      TweenLite.to(el, speed/4, {
+        opacity: 1, 
+        delay: i * speed + general_delay
+      })
+      if(i + 1 < $("#animation1 > g").length){
+        TweenLite.to(el, speed/4, {
+          opacity: 0, 
+          delay: i * speed + general_delay + 3 * speed/4
+        })
+      }
+    }
+  }
+
+  ////////////////////////////////////////////////////////////
+
   Cryptoloji.states.welcome = {
     enter: function () {
 
@@ -14,6 +53,8 @@
       $('#body_landing').children().each(function(i, e){
         $(e).css({opacity:0})
       })
+
+      triggerLogoAnimation()
 
       setTimeout(function(){
         $('#body_landing').transition({opacity:1, duration:250})
