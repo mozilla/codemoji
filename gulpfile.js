946062734616d63dd0a754dcd89ad021a85abe5f
Christopher De Cairos <cade@mozillafoundation.org>
2016-06-30 14:13:54 -0400

    Modify gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -126,7 +126,23 @@ gulp.task('generate-favicon', function(done) {
 gulp.task('finalhtml', ['copy:assets'], function(){
 
   var htmlminopt = {collapseWhitespace: true, removeComments:true}
-  var env = require('./env.json');
+  var env;
+  var herokuEnv = `${process.env.ENV_DIR}/DOMAIN`
+
+  if (fs.existsSync('./env.json')) {
+    env = require('./env.json')
+  }
+  else if (fs.existsSync(herokuEnv)) {
+    env = {
+      domain: fs.readFileSync(herokuEnv, {
+        encoding: 'utf8'
+      })
+    }
+  } else {
+    env = {
+      domain: 'http://localhost:8080'
+    }
+  }
 
   gulp.src('public/share.html')
     .pipe(processhtml({
